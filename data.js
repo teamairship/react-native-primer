b60e42407febdca48a62e9130650ac061aeccb10
Alex Hinson <alex@teamairship.com>
2017-10-28 09:19:06 -0500

    Add sample data

---
+++ data.js
@@ -0,0 +1,19 @@
+const DATA = [
+  {
+    title: 'Thing not to do #1'
+  },
+  {
+    title: 'Thing not to do #2'
+  },
+  {
+    title: 'Thing not to do #3'
+  },
+  {
+    title: 'Thing not to do #4'
+  },
+  {
+    title: 'Thing not to do #5'
+  }
+];
+
+export default DATA;
